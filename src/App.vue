20b279fc2cbe941779cf1dc22ed0d55cfe27ea47
Zhuzhiguo <385603706@qq.com>
2020-07-20 16:38:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="app">
+    <div>
+      <van-button type="primary" @click="request">Button</van-button>
+    </div>
+    <van-divider>
+      <p>{{swiperdata}}</p>
+    </van-divider>
+    <van-divider>
+      <p>{{catitems}}</p>
+    </van-divider>
+    <van-divider>
+      <p>{{floordata}}</p>
+    </van-divider>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'app',
+  data () {
+    return {
+      swiperdata: '',
+      catitems: '',
+      floordata: '',
+    }
+  },
+  methods: {
+    request () {
+      // this.getSwiperdata()
+      // this.getCatitems()
+      // this.getFloordata()
+      Promise.all([this.getSwiperdata(), this.getCatitems(), this.getFloordata()]).then((results) => {
+        // console.log(results);
+        var [swiperdata, catitems, floordata] = results
+        this.swiperdata = swiperdata
+        this.catitems = catitems
+        this.floordata = floordata
+        console.log(swiperdata)
+        console.log(catitems)
+        console.log(floordata)
+      })
+    },
+    // async getSwiperdata () {
+    //   const { data: res } = await this.$http({
+    //     url: this.$http.adornUrl('/home/swiperdata'),
+    //     method: 'get'
+    //   })
+    //   console.log(res)
+    // },
+    // async getCatitems () {
+    //   const { data: res } = await this.$http({
+    //     url: this.$http.adornUrl('/home/catitems'),
+    //     method: 'get'
+    //   })
+    //   console.log(res)
+    // },
+    // async getFloordata () {
+    //   const { data: res } = await this.$http({
+    //     url: this.$http.adornUrl('/home/floordata'),
+    //     method: 'get'
+    //   })
+    //   console.log(res)
+    // },
+    getSwiperdata () {
+      return this.$http({
+        url: this.$http.adornUrl('/home/swiperdata'),
+        method: 'get'
+      })
+    },
+    getCatitems () {
+      return this.$http({
+        url: this.$http.adornUrl('/home/catitems'),
+        method: 'get'
+      })
+    },
+    getFloordata () {
+      return this.$http({
+        url: this.$http.adornUrl('/home/floordata'),
+        method: 'get'
+      })
+    }
+    // getSwiperdata () {
+    //   this.$http({
+    //     url: this.$http.adornUrl('/home/swiperdata'),
+    //     method: 'get'
+    //   }).then(({ data }) => {
+    //     console.log(data)
+    //   })
+    // },
+    // getCatitems () {
+    //   this.$http({
+    //     url: this.$http.adornUrl('/home/catitems'),
+    //     method: 'get'
+    //   }).then(({ data }) => {
+    //     console.log(data)
+    //   })
+    // },
+    // getFloordata () {
+    //   this.$http({
+    //     url: this.$http.adornUrl('/home/floordata'),
+    //     method: 'get'
+    //   }).then(({ data }) => {
+    //     console.log(data)
+    //   })
+    // }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/utils/httpRequest.js
@@ -0,0 +1,59 @@
+
+import axios from 'axios'
+import qs from 'qs'
+import merge from 'lodash/merge'
+
+// import Cookies from 'js-cookie'
+
+// const lang = Vue.cookie.get('language') === 'en' ? 'en-US' : 'zh-CN'
+const http = axios.create({
+  timeout: 1000 * 30,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Headers': 'Authorization,Origin, X-Requested-With,Content-Type, Accept,Access-Token',
+    // "Access-Control-Allow-Methods": "GET,POST",
+    // 'Access-Control-Allow-Credentials': 'true',
+    // 'Access-Control-Expose-Headers': '*'
+  }
+})
+
+/**
+ * 请求地址处理
+ * @param {*} actionName action方法名称
+ */
+http.adornUrl = (actionName) => {
+  // 非生产环境 && 开启代理, 接口前缀统一使用[/proxyApi/]前缀做代理拦截!
+  return '/api/public/v1' + actionName
+}
+
+/**
+ * get请求参数处理
+ * @param {*} params 参数对象
+ * @param {*} openDefultParams 是否开启默认参数?
+ */
+http.adornParams = (params = {}, openDefultParams = true) => {
+  var defaults = {
+    't': new Date().getTime()
+  }
+  return openDefultParams ? merge(defaults, params) : params
+}
+
+/**
+ * post请求数据处理
+ * @param {*} data 数据对象
+ * @param {*} openDefultdata 是否开启默认数据?
+ * @param {*} contentType 数据格式
+ *  json: 'application/json; charset=utf-8'
+ *  form: 'application/x-www-form-urlencoded; charset=utf-8'
+ */
+http.adornData = (data = {}, openDefultdata = true, contentType = 'json') => {
+  var defaults = {
+    't': new Date().getTime()
+  }
+  data = openDefultdata ? merge(defaults, data) : data
+  return contentType === 'json' ? JSON.stringify(data) : qs.stringify(data)
+}
+
+export default http
